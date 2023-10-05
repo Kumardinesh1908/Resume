4a45e62d3dd1038adbc848e8964e8516de56b741
Dinesh Kumar <Kumardinesh1908@gmail.com>
2023-10-05 08:10:41 +0530

    styles of body to keep particles in screen size

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,11 +2,11 @@
 let loader = document.querySelector(".loader-container");
 
 window.addEventListener("load", () => {
-  loader.classList.add("disappear");
+    loader.classList.add("disappear");
 });
 window.addEventListener("load", () => {
-  setTimeout(function () {
-    $(".no-scroll-preload").css("overflow", "visible");
-  }, 1000);
-  $(".loader-container").fadeOut(2500);
+    setTimeout(function () {
+        $(".no-scroll-preload").css("overflow", "visible");
+    }, 1000);
+    $(".loader-container").fadeOut(2500);
 })
