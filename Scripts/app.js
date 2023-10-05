853ebaf1d6d022144432840231061eb5b378924d
Dinesh Kumar <Kumardinesh1908@gmail.com>
2023-10-05 09:08:16 +0530

    updated code format

--- Scripts/app.js
+++ Scripts/app.js
@@ -195,12 +195,11 @@ const scrollToTop = () => {
 $(window).scroll(function () {
     //The button will be hidden until we scroll more than the window's height
     if ($(window).scrollTop() < $(window).height()) {
-      $("#btnScrollToTop").css("visibility", "hidden");
+        $("#btnScrollToTop").css("visibility", "hidden");
     } else {
-      $("#btnScrollToTop").css("visibility", "visible");
+        $("#btnScrollToTop").css("visibility", "visible");
     }
-  });
-  
+});
 
 
 // Contact-Form.js
@@ -208,30 +207,30 @@ $(window).scroll(function () {
 var form = document.getElementById("contact-form");
 
 async function handleSubmit(event) {
-  event.preventDefault();
-  var status = document.getElementById("contact-form-status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      'Accept': 'application/json'
-    }
-  }).then(response => {
-    if (response.ok) {
-      status.innerHTML = "Message sent successfully!";
-      form.reset()
-    } else {
-      response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-        } else {
-          status.innerHTML = "Oops! There was a problem submitting your form"
+    event.preventDefault();
+    var status = document.getElementById("contact-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
         }
-      })
-    }
-  }).catch(error => {
-    status.innerHTML = "Oops! There was a problem submitting your form"
-  });
+    }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "Message sent successfully!";
+            form.reset()
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                    status.innerHTML = "Oops! There was a problem submitting your form"
+                }
+            })
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+    });
 }
 form.addEventListener("submit", handleSubmit)
