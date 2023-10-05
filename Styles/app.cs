c6bbcc30f61fe912e76732b0aa4537fd77f25376
Dinesh Kumar <Kumardinesh1908@gmail.com>
2023-10-05 08:32:19 +0530

    Styling of Scrollbar

--- Styles/app.cs
+++ Styles/app.cs
@@ -3,3 +3,31 @@
   src: url(/fonts/Agustina.woff);
 }
 
+/*
+---------------------------
+SCROLLBAR
+---------------------------
+*/
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px #bb86fc;
+  border-radius: 10px;
+  background: #f1f1f1;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  max-height: 10px;
+  -webkit-box-shadow: inset 0 0 6px #bb86fc;
+  background: #bb86fc;
+  transition: 0.3s;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #ca9eff;
+
+}
