bf7bea4f33a22ce87bfb7281b6893d1b713ce97e
Dinesh Kumar <Kumardinesh1908@gmail.com>
2023-10-06 08:01:34 +0530

    Added skills.html

--- Scripts/app.js
+++ Scripts/app.js
@@ -29,6 +29,7 @@ let header = $(`<nav class="navbar navbar-expand-lg fixed-top dark-theme" id="na
                 <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto" id="navbar-content">
                         <li class="nav-item active nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
+                        <li class="nav-item active nav-item-hover"><a class="nav-link" href="skills.html">Skills</a></li>
                         <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
                         <li class="nav-item active nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
                         <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
